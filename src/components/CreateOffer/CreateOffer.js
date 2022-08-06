684f4392cdf04dcd656e84ae5885bdf27075565f
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2022-08-06 11:28:53 +0300

    Create new offer functionality

--- src/components/CreateOffer/CreateOffer.js
+++ src/components/CreateOffer/CreateOffer.js
@@ -1,17 +1,67 @@
+import { useContext, useState } from "react"
+import { AuthContext } from "../../contexts/authContext"
 import "./CreateOffer.css"
+import * as requester from "../../services/requester"
 
 
 export const CreateOffer = () => {
 
+    const { user } = useContext(AuthContext)
+    const accessToken = user?.accessToken
+    const ownerName = user?.name
+
+    const [values, setValues] = useState({
+        category: 'accounting',
+        possition: '',
+        summary: '',
+        vacancies: 1,
+        salary: '',
+        requirements: '',
+        imageUrl: '',
+        ownerName
+    })
+
+    const changeHandler = (e) => {
+        setValues((oldValues) => (
+            { ...oldValues, [e.target.name]: e.target.value }
+        ))
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await requester.post("/data/jobOffers", values, accessToken)
+            console.log(response);
+            console.log(`Offer added successfuly`);
+        } catch (error) {
+
+        }
 
 
+    }
 
     return <section id="create-page" className="auth">
 
-        <form id="create" >
+        <form id="create" onSubmit={submitHandler}>
             <div className="create-container">
-                <h5 className="create-company">The job offer is created by:</h5>
+                <h5 className="create-company">The job offer is created by: <span id="company-name">{user?.name}</span></h5>
                 <h1>Create Game</h1>
+
+                <label htmlFor="category" >Category</label>
+                <select name="category" value={values.category} onChange={changeHandler}>
+                    <option value="accounting">Accounting</option>
+                    <option value="administration">Administration</option>
+                    <option value="marketing">Marketing</option>
+                    <option value="hotelsAndRestaurants">Hotels and Restaurants</option>
+                    <option value="healthAndCare">Health and care</option>
+                    <option value="construction">Construction</option>
+                    <option value="finance">Finance</option>
+                    <option value="manufacturing">Manufacturing</option>
+                    <option value="transportation">Transportation</option>
+                    <option value="retail">Retail</option>
+                    <option value="realEstate">Real Estate</option>
+                </select>
+
                 <label htmlFor="possition">Job possition:</label>
                 <input
                     className="create-input"
@@ -19,11 +69,16 @@ export const CreateOffer = () => {
                     id="possition"
                     name="possition"
                     placeholder="Enter job possition here"
-                // value={values.title}
-                // onChange={changeHandler}
+                    value={values.possition}
+                    onChange={changeHandler}
                 />
-                <label htmlFor="description">Job description:</label>
-                <textarea name="description" id="description" />
+                <label htmlFor="summary">Job description:</label>
+                <textarea
+                    name="summary"
+                    id="summary"
+                    value={values.summary}
+                    onChange={changeHandler}
+                />
 
                 <label htmlFor="vacancies">Available job possitions:</label>
                 <input
@@ -32,7 +87,8 @@ export const CreateOffer = () => {
                     name="vacancies"
                     min={1}
                     placeholder={1}
-
+                    value={values.vacancies}
+                    onChange={changeHandler}
                 />
 
                 <label htmlFor="salary">Salary:</label>
@@ -42,22 +98,26 @@ export const CreateOffer = () => {
                     id="salary"
                     name="salary"
                     placeholder={`1 000 - 1 500 $`}
-                // value={values.maxLevel}
-                // onChange={changeHandler}
+                    value={values.salary}
+                    onChange={changeHandler}
                 />
 
                 <label htmlFor="requirements">Requirements:</label>
-                <textarea name="requirements" id="requirements" />
+                <textarea
+                    name="requirements"
+                    id="requirements"
+                    value={values.requirements}
+                    onChange={changeHandler} />
 
-                <label htmlFor="game-img">Image:</label>
+                <label htmlFor="company-logo">Image:</label>
                 <input
                     className="create-input"
                     type="text"
                     id="imageUrl"
                     name="imageUrl"
                     placeholder="Upload a photo..."
-                // value={values.imageUrl}
-                // onChange={changeHandler}
+                    value={values.imageUrl}
+                    onChange={changeHandler}
                 />
 
 
